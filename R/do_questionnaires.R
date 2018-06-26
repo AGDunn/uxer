327e53b708ad57c4f49c26c06f52124286c80933
AGDunn <agd1983@gmail.com>
2018-06-26 08:14:39 +0100

    score_sus(): added user_id param.

--- R/do_questionnaires.R
+++ R/do_questionnaires.R
@@ -1,15 +1,23 @@
+# ###############################################################
 # TODO BEFORE RUNNING DOCUMENTATION:
 #  1/ add subscales
 #  2/ add rescale yes/no option
 #  3/ make calculation non-silly (main scale route via subscales)
 # ###############################################################
-#' Calculate participants' scores on SUS scales.
-#' @param 
+# Lewis and Sauro (2009) have 2 subscales:
+#   `Usable' = 1, 2, 3, 5, 6, 7, 8, 9 [rescale factor = 3.125]
+#   `Learnable' = 4, 10 [rescale factor = 12.5]
+# ###############################################################
+
+#' Calculate participants' SUS questionnaire scores.
+#' @param myData, no default; data frame of questionnaire results; see example
+#'   for naming convention expected.
+#' @param user_id, default TRUE; does the data frame have a column for user ID?
 #' @keywords 
 #' @concept questionnaire, scale
 #' score_sus()
-score_sus <- function(myData){
-if (length(myData) == 11){ # won't run if 
+score_sus <- function(myData, user_id=TRUE){
+if (ncol(myData) == 10 + user_id){ # won't run if 
     myData %>%               # wrong number of columns
       mutate(
         normalQ=(            # total score for 
