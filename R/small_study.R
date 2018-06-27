5ba74a365aa96ea6c80760d1ef45d0c593c75c0e
AGDunn <agd1983@gmail.com>
2018-06-27 09:01:54 +0100

    Added basic documentation for small_study data.

---
+++ R/small_study.R
@@ -0,0 +1,12 @@
+#' Responses to an SUS questionnaire.
+#'
+#' 8 fictitious participants responded to a standard SUS questionnaire
+#' as part of usability testing a made-up software package.  Nothing 
+#' sophisticated was done to imagine up these data.
+#'
+#' @format A tibble of 8 rows and 11 variables.
+#' \describe{
+#'   \item{participant}{ID variable for participant in study.}
+#'   \item{Q1 to Q10}{responses to standard SUS questions, range 1--5}
+#' }
+#' @source This is fictional data.
