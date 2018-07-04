e0b9a5e7774710eaf6b39febda0c6cbec11492dc
AGDunn <agd1983@gmail.com>
2018-07-04 21:58:57 +0100

    Made and documented basic TAM function.

--- R/do_questionnaires.R
+++ R/do_questionnaires.R
@@ -57,13 +57,41 @@ score_sus <- function(myData, user_id=TRUE, rescale=TRUE, subscales=FALSE){
 
 #' Calculate participants' scores on technology acceptance model
 #'
-#' Calculate participants' scores on full technology acceptance model.
-#' 
+#' Calculate participants' scores on full technology acceptance model.  There
+#'   are two subscales: `perceived usefulness' and `perceived ease of use'.
+#'   Currently assumes participant ID, then usefulness scale, then ease scale.
+#'   It's based just off the original paper and no later extensions (yet).
+#'
+#' @param myData a data frame with (optional) participant ID,
+#'   then (mandatory) usefulness scale questions followed by
+#'   (mandatory) ease-of-use scale questions.
+#' @param user_id boolean for presence or absence of user ID column as first
+#'   column.
 #' @concept questionnaire, scale
 #' @references Davis, Fred D. (1989) ``Perceived Usefulness, Perceived Ease
 #'   of Use, and User Acceptance of Information'' \emph{MIS Quarterly} 13 (3)
 #' @export
-score_tam <- function(myData){
+score_tam <- function(myData, user_id=TRUE){
+  # coerce to tibble in case it matters
+  myData <- as_tibble(myData)
+  
+  # rename variables for sake of later selection
+  usef_start <- user_id + 1
+  ease_start <- user_id + 7
+  names(myData)[usef_start:(usef_start + 5)] <- 
+    c("u1", "u2", "u3", "u4", "u5", "u6")
+  names(myData)[ease_start:(ease_start + 5)] <- 
+    c("e1", "e2", "e3", "e4", "e5", "e6")
+  
+  output_holder <- myData %>% mutate(
+    usefulness = u1 + u2 + u3 + u4 + u5 + u6,
+    ease = e1 + e2 + e3 + e4 + e5 + e6
+    ) %>% select(usefulness, ease)
+  if (user_id){
+    output_holder <- output_holder %>%
+      add_column(participant=pull(myData[, 1]), .before = 1)
+  } 
+  return(output_holder)
 }
 
 # 00000000000000000000000000000000000000000000000000000000000000000000000000000
